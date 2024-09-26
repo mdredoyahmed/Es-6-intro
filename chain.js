0a0100e23ec59953831b8aa7fcf0aa9a592ec50d
Md Hridoy Ahmed <95656155+mdredoyahmed@users.noreply.github.com>
2024-09-27 02:02:59 +0600

    file chang

--- chain.js
+++ chain.js
@@ -2,3 +2,29 @@
 const data =[{id:1, name:'abul', address: 'ruppur'}];
 
 console.log(data[0].address);
+
+const products ={
+    count: 4000,
+    data: [
+        {id:1 , name:'lenova laptop' , price: 4500 },
+        { id:2 , name:'macbook ' , price: 300}
+    ]
+}
+console.log(products.data[1].name);
+
+// user
+
+const user = {
+    id: 3001,
+    name:'ashik',
+    address:{
+        street: {
+            first:'uttor side',
+            second:'poribag er goli',
+            third:'no doria',
+        },
+        city:'dhaka',
+    }
+}
+
+console.log(user.address.street.second);
